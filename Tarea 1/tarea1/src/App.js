b164b6e81f33f46e2088cde6a894ac27edcaaa29
David Rojas <david.davidrojasobando.rojas@gmail.com>
2022-04-15 11:12:18 -0600

    Tarea 1

---
+++ Tarea 1/tarea1/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <h1 classname="text-3xl font-bold underline">
+      Nombre: David Rojas Obando
+      <br/>Carné: B96873
+    </h1>
+  );
+}
+
+export default App;
